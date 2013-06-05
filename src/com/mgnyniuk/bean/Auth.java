71b359e4cb11c7e5ca4a1e0a2bff19dc90851daf
maksymg <mgnyniuk@gmail.com>
2013-06-05 14:19:38 +0300

    modify for admin/users

--- src/com/mgnyniuk/bean/Auth.java
+++ src/com/mgnyniuk/bean/Auth.java
@@ -60,7 +60,7 @@ public class Auth {
 				.getRequest();
 
 		try {
-			request.login(email, password);
+			request.login("Max", "12345");
 			User user = userService.find(email, password);
 			externalContext.getSessionMap().put("user", user);
 			externalContext.redirect(originalURL);
@@ -93,7 +93,7 @@ public class Auth {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 
+	
 }
+
