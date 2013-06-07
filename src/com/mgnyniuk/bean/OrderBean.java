3003f1a9fb6f37ca7bd9af1ac4c4d260dbe2b025
maksymg <mgnyniuk@gmail.com>
2013-06-07 17:21:53 +0300

    Revert "Revert "бб""
    This reverts commit 3d8e5765ae0c42979b8feb1136ab3316bc40d7ee.

--- src/com/mgnyniuk/bean/OrderBean.java
+++ src/com/mgnyniuk/bean/OrderBean.java
@@ -6,7 +6,10 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
+import com.mgnyniuk.ejb.OrderService;
 import com.mgnyniuk.jpa.Book;
 import com.mgnyniuk.jpa.Order;
 import com.mgnyniuk.jpa.User;
@@ -16,11 +19,17 @@ import com.mgnyniuk.jpa.User;
 public class OrderBean {
 
 	private Order order;
+	private OrderService orderService;
 
 	@PostConstruct
 	private void init() {
-		System.out.println(FacesContext.getCurrentInstance()
-				.getExternalContext().getSessionMap().get("user"));
+		try {
+			orderService = (OrderService) InitialContext
+					.doLookup("java:module/OrderService");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void createOrder(List<Book> bookList) {
@@ -30,6 +39,7 @@ public class OrderBean {
 		order.setBookList(bookList);
 		order.setDescription("Order");
 		order.setUser(currentUser);
+		orderService.add(order);
 	}
 
 }
